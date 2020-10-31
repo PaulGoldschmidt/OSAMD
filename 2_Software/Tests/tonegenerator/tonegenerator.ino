17305cc1108a051204dfab3489542db4e69bbb4b
Paul Goldschmidt <paul.goldschmidt@web.de>
2020-10-31 09:39:49 +0100

    created first test

---
+++ 2_Software/Tests/tonegenerator/tonegenerator.ino
@@ -0,0 +1,17 @@
+const int buzzer = 4; //buzzer to arduino pin 9
+
+
+void setup(){
+ 
+  pinMode(buzzer, OUTPUT); // Set buzzer - pin 9 as an output
+
+}
+
+void loop(){
+ 
+  tone(buzzer, 440); // Send 1KHz sound signal...
+  delay(1000);        // ...for 1 sec
+  noTone(buzzer);     // Stop sound...
+  delay(1000);        // ...for 1sec
+  
+}
