5e14391f768519461b0341703e3b7de248cff817
Paul Goldschmidt <paul.goldschmidt@web.de>
2020-11-08 15:22:40 +0100

    Added Delay and removed unnecessary comments

--- 2_Software/Tests/MQ135-Test/MQ135-Test.ino
+++ 2_Software/Tests/MQ135-Test/MQ135-Test.ino
@@ -15,12 +15,11 @@ void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
 }
-
 // the loop routine runs over and over again forever:
 void loop() {
   // read the input on analog pin 0:
   int sensorValue = analogRead(A0);
-  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
   // print out the value you read:
   Serial.println(sensorValue);
+  delay(1000);
 }
