e6d9b041ab283c2a4775daad9edc9d41469c3492
Paul Goldschmidt <paul.goldschmidt@web.de>
2020-11-09 20:37:17 +0100

    Small corrections in tabulation

--- 2_Software/Arduino/main/main.ino
+++ 2_Software/Arduino/main/main.ino
@@ -9,7 +9,7 @@ const int buzzer = 5; //buzzer to arduino pin 5
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
-Setup_I2C();
+  Setup_I2C();
   pinMode(buzzer, OUTPUT); // Set buzzer - pin 5 as an output
 }
 
